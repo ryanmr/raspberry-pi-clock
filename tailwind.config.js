72af5a466e5a1bb9c4ff02031df684eda69d1dfe
Ryan Rampersad <ryan.rampersad@gmail.com>
2024-02-14 18:53:31 -0600

    Try fancy clamp scaling font sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        "responsive-xl": "clamp(12.5rem, 4.318rem + 8.182vw, 16rem)",
+        "responsive-lg": "clamp(1.5rem, 1.091rem + 0.909vw, 2rem)",
+      },
+    },
   },
   plugins: [],
 };
